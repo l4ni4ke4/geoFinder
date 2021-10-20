c8188ebcc5e952edbe8953fb6a12aa1adef8ffd8
Berat Soyakça <mberatsoyakca@gmail.com>
2021-10-20 12:12:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  GoogleMap,
+  LoadScript,
+  StreetViewPanorama,
+} from "@react-google-maps/api";
+
+import "./App.css"
+
+const mapContainerStyle = {
+  width: "700px",
+  height: "500px"
+};
+
+const center = {
+  lat:41.106196,
+  lng:28.803581
+};
+
+const mapOptions = {
+  // disableDefaultUI: true,
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  return (<>
+
+  <h1>INF503 Proje keyff</h1>
+
+  <LoadScript
+  googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+
+    {/* soldaki harita */}
+    <div id='mapLeft'>
+      <GoogleMap mapContainerStyle={mapContainerStyle} zoom ={8} center={center} options ={mapOptions}>
+      </GoogleMap>
     </div>
-  );
+
+    {/* Streetview (sağdaki harita) */}
+    <div id='mapRight'>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={14}
+        center={center}
+      >
+        <StreetViewPanorama
+              position={center}
+              visible={true}
+            />
+      </GoogleMap>
+    </div>
+
+  </LoadScript>
+
+  </>);
 }
 
 export default App;
