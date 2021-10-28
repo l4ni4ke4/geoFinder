784ce80692bceceb16f1e1b153cbe4679dab23a0
Berat Soyakça <mberatsoyakca@gmail.com>
2021-10-28 12:45:47 +0300

    migrate random location functions from GamePage to RoundPlay

--- src/pages/Game/GamePage.js
+++ src/pages/Game/GamePage.js
@@ -1,10 +1,8 @@
 import React from "react";
-import {useState, useEffect} from "react";
+import {useState} from "react";
 import {
     useJsApiLoader
   } from "@react-google-maps/api";
-
-import randomStreetView from 'random-streetview';
 
 import "./GamePage.css";
 import RoundPlay from "../../components/RoundPlay/RoundPlay";
@@ -13,31 +11,7 @@ const libraries = ["places","drawing"]; // for useLoadScript below
 
 
 
-/* const StreetviewPosition = {
-  lat:41.106196,
-  lng:28.803581
-}; */
-
-var returnLocation = [];
-
-async function generateRandomStreetView() {
-  Promise.resolve(await randomStreetView.getRandomLocation()).then(value => {
-    returnLocation = value;
-  });
-}
-
-
-
 export default function GamePage() {
-
-  useEffect(() => {
-    generateRandomStreetView();
-  }, []);
-
-  const StreetviewPosition = {
-    lat: returnLocation[0],
-    lng: returnLocation[1]
-  };
 
   // Add google scripts
   const {isLoaded, loadError} = useJsApiLoader ({
