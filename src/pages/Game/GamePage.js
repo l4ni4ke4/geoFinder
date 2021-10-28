b001b93e4e6d8689834a527ef0563c96fb09d05a
Arman Yavuz <e170503023@stud.tau.edu.tr>
2021-10-28 13:39:01 +0300

    page to component migration error fixes

--- src/pages/Game/GamePage.js
+++ src/pages/Game/GamePage.js
@@ -1,28 +1,10 @@
 import React from "react";
-import {useState} from "react";
-import {
-    useJsApiLoader
-  } from "@react-google-maps/api";
-
 import "./GamePage.css";
 import RoundPlay from "../../components/RoundPlay/RoundPlay";
-
-const libraries = ["places","drawing"]; // for useLoadScript below
 
 
 
 export default function GamePage() {
-
-  // Add google scripts
-  const {isLoaded, loadError} = useJsApiLoader ({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries
-  });
-  
-
-  // for loading errors
-  if (loadError) return "Error loading maps";
-  if (!isLoaded) return "Loading maps";
 
     return (<>
       <RoundPlay/>
