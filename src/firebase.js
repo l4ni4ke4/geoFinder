b69ee9949e19c1041a108b425cef0b5bc11655ca
Arman Yavuz <e170503023@stud.tau.edu.tr>
2021-11-29 17:41:54 +0300

    >small firebaseConfig fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,13 @@
 import firebase from "firebase";
 
-const firebaseConfig = process.env.FIREBASE_CONFIG;
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_CONFIG,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
 
 const app = firebase.initializeApp(firebaseConfig);
 const auth = app.auth;
