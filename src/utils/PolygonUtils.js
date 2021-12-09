cd0b29bf4dbd7f756edbb1fcfc8ea1bd72175a30
SerdarPe <61140890+SerdarPe@users.noreply.github.com>
2021-12-09 12:01:31 +0300

    Update translation functions

--- src/utils/PolygonUtils.js
+++ src/utils/PolygonUtils.js
@@ -1,34 +1,27 @@
 import { getAllCountries } from "./Polygons";
 
-let enums = []
-let enumsCreated = 0;
-
-function createEnums(){
-    for (var i = 0; i < getAllCountries().length; i++) {
-        var e = {country:getAllCountries()[i].country, value:i};
-        enums.push(e);
-    }
-    enumsCreated = 1;
-}
-
-export function getEnums(){
-    if(enumsCreated === 0){
-        createEnums();
-    }
-    return enums;
-}
-
 /**
  * Translate enums into Country Polygons
  * @param {Object[]} num_arr Array of numbers that includes enums.
  * @returns {Object[]} Merged Polygons
  */
-export function translateToPolygons(num_arr){
+export function translateToPolygonsFromEnums(num_arr){
     var Polygons = [];
     for(var i = 0; i < num_arr.length; i++){
-        //var countryName = enums.find(e => e.value === num_arr[i]).country;
-        //Polygons.push(getAllCountries().find((c => c.country === countryName)).polygons);
         Polygons.push(getAllCountries()[num_arr[i]].polygons);
+    }
+    return Polygons;
+}
+
+/**
+ * Translate country codes into Country Polygons
+ * @param {Object[]} code_arr - Array of country codes (strings).
+ * @returns {Object[]} Merged Polygons
+ */
+export function translateToPolygonsFromCodes(code_arr){
+    var Polygons = [];
+    for(var i = 0; i < code_arr.length; i++){
+        Polygons.push(getAllCountries().find((c => c.code === code_arr[i])).polygons);
     }
     return Polygons;
 }
