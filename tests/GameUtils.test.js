d9397095bd2d88178d4e6530c222ce477715fcab
Serdar Pehlivan <61140890+SerdarPe@users.noreply.github.com>
2021-12-30 14:46:19 +0300

    Add tests for getScore()

--- tests/GameUtils.test.js
+++ tests/GameUtils.test.js
@@ -1,4 +1,4 @@
-import {calculateDistance} from '../src/utils/GameUtils'
+import {getScore, calculateDistance} from '../src/utils/GameUtils'
 
 test('66N 1E -- 60N 1E ~= 668.77 km', () => {
     //Distance calculated from Google Earth results to be 668.77 km
@@ -22,4 +22,23 @@ test('52N 10W -- 60N 10E ~= 1523.31 km', () => {
     var distance = calculateDistance(52, 60, -10, 10)
     expect(distance).toBeLessThanOrEqual(1523.31 * 1.005)                    //1530.92 km
     expect(distance).toBeGreaterThanOrEqual(1523.31 * 0.995)                 //1515.69 km
+});
+
+test('Distance 1000 - 4800 Points', () => {
+    //Default maxDistance is 5000
+    var score = getScore(1000, 5000)
+    expect(score).toBeLessThanOrEqual(4800 + 1)
+    expect(score).toBeGreaterThanOrEqual(4800 - 1)
+});
+
+test('Distance 29 - 5000 Points', () => {
+    //Default maxDistance is 5000
+    var score = getScore(29, 5000)
+    expect(score).toBe(5000)
+});
+
+test('Distance 5001 - 0 Points', () => {
+    //Default maxDistance is 5000
+    var score = getScore(5001, 5000)
+    expect(score).toBe(0)
 });
