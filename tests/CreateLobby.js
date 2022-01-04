58372506efaf479e595497844c9bd58e650b12c6
Serdar Pehlivan <61140890+SerdarPe@users.noreply.github.com>
2022-01-04 15:50:35 +0300

    adapt current selenium tests to connect geofinder website

--- tests/CreateLobby.js
+++ tests/CreateLobby.js
@@ -12,29 +12,26 @@ var driver = new webdriver.Builder()
 
 // This doesnt work for some reason idk why, fucking shitscript
 const logins = {
-    email: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    pass: process.env.REACT_APP_FIREBASE_PROJECT_ID
+    email: process.env.FIREBASE_TEST_EMAIL,
+    pass: process.env.FIREBASE_TEST_PASS
 };
 
 
-async function createLobby() {
+async function createLobbyTest() {
 
   try {
-    await driver.get('http://localhost:3000/');     //go to geofinder login page
-
-    await driver.findElement(By.xpath("//input[@placeholder='E-mail Address']")).sendKeys();  //enter random email used to register
-    await driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(randomPass);        //enter random password used to register
-    await driver.findElement(By.xpath("//button[@class='btn-login']")).click();     //Click login button
-    await driver.wait(until.urlContains('Home'), 1000 * 5);     //wait for home page
+    await driver.get('https://geofinder-9a266.web.app/');     //go to geofinder login page
 
     await driver.findElement(By.xpath("//input[@placeholder='E-mail Address']")).sendKeys("kek@kek.com");  //enter email
     await driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("kekkek");        //enter password
     await driver.findElement(By.xpath("//button[@class='btn-login']")).click();     //Click login button
     await driver.wait(until.urlContains('Home'), 1000 * 5);     //wait for home page
-    await driver.findElement(By.xpath("//img[@class='mp-image image']")).click();   //Click MP button
-    await driver.findElement(By.xpath("//button[@class='btn btn-success']")).click();   //Create new lobby
-    await driver.findElement(By.xpath("//h1['Multiplayer Lobby']"));    //This text shows up when the lobby is created
 
+    await driver.findElement(By.xpath("//div[@class='play-box-multiplayer-left']")).click();   //Click MP button
+    await driver.wait(until.elementLocated(By.xpath("//button['Create new Lobby']")));
+    await driver.findElement(By.xpath("//button['Create new Lobby']")).click();   //Click Create new lobby
+    await driver.findElement(By.xpath("//p['Multiplayer Lobby']"));    //This text shows up when the lobby is created
+    console.log("CREATELOBBY SUCCESS");
   } 
   finally {
     await driver.quit();
@@ -42,4 +39,4 @@ async function createLobby() {
 };
 
 
-createLobby()
+createLobbyTest()
--- tests/LoginLocal.js
+++ tests/LoginLocal.js
@@ -9,30 +9,38 @@ var driver = new webdriver.Builder()
                  .withCapabilities(webdriver.Capabilities.chrome())
                  .build();
 
+
 async function testLocal() {
   //6 chars random name
   var randomName = Math.random().toString(36).substr(2, 6);
   //8 chars random password
   var randomPass = Math.random().toString(36).substr(2, 8);
+  driver.manage()
+        .setTimeouts({
+        'pageLoad': 10000,
+        'script': 60000,
+        'implicit': 5000
+      });
   try {
-    await driver.get('http://localhost:3000/');     //go to geofinder login page
+    await driver.get('https://geofinder-9a266.web.app/');     //go to geofinder login page
     
     await driver.findElement(By.xpath("//a[@href='/register']")).click();   //go to register page
-    await driver.wait(until.urlContains('register'), 1000 * 5);     //wait until register page comes up
+    //await driver.wait(until.urlContains('register'), 1000 * 5);     //wait until register page comes up
     await driver.findElement(By.xpath("//input[@placeholder='Full Name']")).sendKeys(randomName);   //enter random name
     await driver.findElement(By.xpath("//input[@placeholder='E-mail Address']")).sendKeys(randomName + "@random.com");  //enter random email
     await driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(randomPass);  //enter random password
     await driver.findElement(By.xpath("//button[@class='register__btn']")).click();     //Click on register button
     
-    await driver.wait(until.urlContains('Home'), 1000 * 5);     //Wait until registeration completed and be redirected to home page
-    await driver.findElement(By.xpath("//button[@id='dropdownMenuButton']")).click();   //To Logout
+    //await driver.wait(until.urlContains('Home'), 1000 * 5);     //Wait until registeration completed and be redirected to home page
+    await driver.findElement(By.xpath("//p[@class='dropdown-toggle']")).click();   //To Logout
     await driver.findElement(By.xpath("//a[@href='/']")).click();   //Now Logout
 
-    await driver.wait(until.urlIs('http://localhost:3000/'), 1000 * 5)  //Wait until login page
+    await driver.wait(until.urlIs('https://geofinder-9a266.web.app/'), 1000 * 5)  //Wait until login page
     await driver.findElement(By.xpath("//input[@placeholder='E-mail Address']")).sendKeys(randomName + "@random.com");  //enter random email used to register
     await driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(randomPass);        //enter random password used to register
     await driver.findElement(By.xpath("//button[@class='btn-login']")).click();     //Click login button
-    await driver.wait(until.urlContains('Home'), 1000 * 5);     //wait for home page
+    //await driver.wait(until.urlContains('Home'), 1000 * 5);     //wait for home page
+    console.log("TESTLOCAL SUCCESS");
   } 
   finally {
     await driver.quit();
